68f89bbda776fb592591c68da0113d0a3e3d052b
Enan Ajmain <3nan.ajmain@gmail.com>
2025-03-19 13:39:04 -0400

    DLv3: finish implementation

--- network/deeplabv3.py
+++ network/deeplabv3.py
@@ -1,7 +1,119 @@
+import torch
 import torch.nn as nn
-from torchvision import models
+import torch.nn.functional as F
 
+class DeepLabv3p(nn.Module):
+    def __init__(self, backbone, classifier):
+        super(DeepLabv3p, self).__init__()
+        self.backbone = backbone
+        self.classifier = classifier
+
+    def forward(self, x):
+        input_shape = x.shape[-2:]
+        features = self.backbone(x)
+        prediction = self.classifier(features)
+        upsampled_prediction = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        features = {'out': upsampled_prediction}
+        return features
+
+class DeepLabHeadV3Plus(nn.Module):
+    def __init__(self, in_channels, low_level_channels, num_classes, aspp_dilate=[12, 24, 36]):
+        super(DeepLabHeadV3Plus, self).__init__()
+        self.project = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, 1, bias=False),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True),
+        )
+
+        # Encoder
+        self.aspp = ASPP(in_channels, aspp_dilate)
+
+        self.classifier = nn.Sequential(
+            nn.Conv2d(304, 256, 3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, 1)
+        )
+        self._init_weight()
+
+    def forward(self, feature):
+        low_level_features = self.project( feature['low_level'] )    # 32x48x129x129
+        low_level_shape = low_level_features.shape[2:]
+        encoded_features = self.aspp(feature['out'])                  # 32x256x17x17
+        upsampled_features = F.interpolate(encoder_output, size=low_level_shape, mode='bilinear', align_corners=False)
+        prediction = self.classifier( torch.cat( [ low_level_features, upsampled_features ], dim=1 ) )
+        return upsampled_prediction
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+class ASPPConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, dilation):
+        modules = [
+            nn.Conv2d(in_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+        super(ASPPConv, self).__init__(*modules)
+
+class ASPPPooling(nn.Sequential):
+    def __init__(self, in_channels, out_channels):
+        modules = [
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),    # paper: stride=2
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+        super(ASPPPooling, self).__init__(*modules)
+
+    def forward(self, x):
+        size = x.shape[-2:]
+        x = super(ASPPPooling, self).forward(x)                     # 32x256x1x1
+        out = F.interpolate(x, size=size, mode='bilinear', align_corners=False) # 32x256x17x17
+        return out
+
+class ASPP(nn.Module):
+    def __init__(self, in_channels, atrous_rates):
+        super(ASPP, self).__init__()
+        out_channels = 256
+        modules = []
+        modules.append(nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),    # paper: stride=2
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)))
+
+        rate1, rate2, rate3 = tuple(atrous_rates)
+        modules.append(ASPPConv(in_channels, out_channels, rate1))
+        modules.append(ASPPConv(in_channels, out_channels, rate2))
+        modules.append(ASPPConv(in_channels, out_channels, rate3))
+        modules.append(ASPPPooling(in_channels, out_channels))
+
+        self.convs = nn.ModuleList(modules)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(5 * out_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(0.1),
+        )
+
+    def forward(self, x):
+        res = []
+        for conv in self.convs:
+            res.append(conv(x))
+        res = torch.cat(res, dim=1)
+        return self.project(res)
 
 if __name__ == '__main__':
     print('hello')
-    backbone = models.resnet101(weights='ResNet101_Weights.IMAGENET1K_V1')
+    in_planes = 2048
+    low_level_planes = 256
+    num_classes = 21
+    aspp_dilate = [12,24,36]    # paper: [6,12,18]
+
+    DeepLabHeadV3Plus(in_planes, low_level_planes, num_classes, aspp_dilate)
