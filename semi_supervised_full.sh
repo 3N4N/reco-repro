65ca25c098b1cb17331e820dff2d434524fb307e
sanjeepan23 <sanjeepan23@gmail.com>
2025-03-23 12:02:35 -0400

    add training bash scripts

---
+++ semi_supervised_full.sh
@@ -0,0 +1,4 @@
+python3 trainers/semi_supervised_full.py \
+  --dataset "pascal" \
+  --data-path "/local/home/sanjee23/dev/ReCo/dataset/pascal" \
+  --val-interval 100
---
+++ supervised_full.sh
@@ -0,0 +1,4 @@
+python3 trainers/supervised_full.py \
+  --dataset "pascal" \
+  --data_root "/local/home/sanjee23/dev/ReCo/dataset/pascal" \
+  --val_interval 100
