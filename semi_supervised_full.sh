3df9049d915e8e2f3ba46c1d78d5e07f9b2377be
sanjeepan23 <sanjeepan23@gmail.com>
2025-04-04 12:15:02 -0400

    update supervised pipeline

--- semi_supervised_full.sh
+++ semi_supervised_full.sh
@@ -1,4 +1,6 @@
 python3 trainers/semi_supervised_full.py \
-  --dataset "pascal" \
-  --data-path "/local/home/sanjee23/dev/ReCo/dataset/pascal" \
-  --val-interval 100
+  --dataset "cityscapes" \
+  --data-path "/local/home/sanjee23/dev/ReCo/dataset/cityscapes" \
+  --val-interval 100 \
+  --num_labeled 20 \
+  --model "deeplabv3_original" 
